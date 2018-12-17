24e3dae696fba46bd4c8b7bcfc845b80ea669162
rafaelnunyez <rafa97burja@gmail.com>
2018-12-17 12:13:12 +0100

    Hecho en clase

---
+++ FragmentoDiversos/app/src/main/java/com/example/rafmar/fragmentodiversos/FragmentoSimple.java
@@ -0,0 +1,44 @@
+package com.example.rafmar.fragmentodiversos;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class FragmentoSimple extends Fragment {
+    int mNum;
+    static FragmentoSimple newInstance(int number) {
+        FragmentoSimple f = new FragmentoSimple();
+        Bundle args = new Bundle();
+        args.putInt("num", number);// Mantenemos el numero para usarlo en cualquier momento.
+        f.setArguments(args);
+        return f;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // obtenemos el nÃºmero que se habia pasado como argumento en
+        // la creaciÃ³n de la instancia
+        mNum = getArguments().getInt("num");
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v  = null;
+        if (mNum % 2 == 0){// dependiendo de si es par o impar mostramos distintos layouts
+            v = inflater.inflate(R.layout.fragment_fragmento_simple, container, false);
+            View tv = v.findViewById(R.id.text);
+            //informamos el nÃºmero de Fragment
+            ((TextView)tv).setText("Fragmento nÃºmero #" + mNum);
+        }
+        else{
+            v = inflater.inflate(R.layout.fragment_simple2, container, false);
+            View tv = v.findViewById(R.id.text2);//informamos el numero de Fragment
+            ((TextView)tv).setText("Fragmento nÃºmero #" + mNum);
+        }
+        return v;
+    }
+}
