985cd4466dbef1c827030f95fdedf912c2d1c774
rafaelnunyez <rafa97burja@gmail.com>
2019-02-09 13:24:52 +0100

    Added correct use of items of the user

--- FinalProject/app/src/main/java/com/example/rafael/finalproject/CountriesScreen.java
+++ FinalProject/app/src/main/java/com/example/rafael/finalproject/CountriesScreen.java
@@ -1,15 +1,15 @@
 package com.example.rafael.finalproject;
 
-import android.app.Activity;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CountriesScreen extends AppCompatActivity {
 
@@ -17,72 +17,62 @@ public class CountriesScreen extends AppCompatActivity {
 
     DatabaseHelper dbHelper;
 
+    ArrayList<String> userCountries;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AdaptadorTitulares adaptador = new AdaptadorTitulares(this);
+        dbHelper = new DatabaseHelper(this);
+        dbHelper.open();
+
+        userCountries = new ArrayList();
+
+        Cursor cursor2 = dbHelper.getItems(
+                SQLSentences.TABLE_COUNTRY_USER_REL,
+                new String[] {SQLSentences.TABLE_COUNTRY_NAME},
+                "user_id = ?",
+                new String[] {String.valueOf(USER)},
+                SQLSentences.TABLE_COUNTRY_ID);
+        while (cursor2.moveToNext()) {
+            userCountries.add(cursor2.getString(0));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userCountries);
 
         USER = getIntent().getIntExtra("USER", 0);
-
-        dbHelper = new DatabaseHelper(this);
 
         setContentView(R.layout.activity_countries_screen);
 
         ListView countryView = (ListView) findViewById(R.id.countryView);
-        countryView.setAdapter(adaptador);
+        countryView.setAdapter(adapter);
+
+        dbHelper.close();
     }
 
-    static class ViewHolder {
-        TextView lblCountry;
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+
+        return true;
     }
 
-    class AdaptadorTitulares extends ArrayAdapter {
-        Activity context;
-
-        AdaptadorTitulares (Activity context) {
-            super(context, R.layout.linear_menu_item);
-            this.context = context;
-        }
-
-        public View getView (int position, View convertView, ViewGroup parent) {
-            String[] columns = {
-                    SQLSentences.TABLE_COUNTRY_USER_REL_COUNTRY_ID,
-                    SQLSentences.TABLE_COUNTRY_NAME
-            };
-
-            View item = convertView;
-            ViewHolder holder;
-
-            Cursor cursor;
-            dbHelper.open();
-            cursor = dbHelper.getItems(SQLSentences.TABLE_COUNTRY_USER_REL, columns, "id = ?",
-                    new String[]{String.valueOf(USER)}, SQLSentences.TABLE_COUNTRY_USER_REL_ID);
-
-            if (cursor.moveToFirst()) {
-
-
-                if (item == null) {
-                    LayoutInflater inflater = context.getLayoutInflater();
-                    item = inflater.inflate(R.layout.linear_menu_item, null);
-                    holder = new ViewHolder();
-                    holder.lblCountry = (TextView) item.findViewById(R.id.Country);
-
-                    item.setTag(holder);
-                } else {
-                    holder = (ViewHolder) item.getTag();
-                }
-
-                holder.lblCountry.setText(cursor.getString(2));
-            }
-
-            if (!cursor.isClosed()) {
-                cursor.close();
-            }
-            
-            dbHelper.close();
-
-            return (item);
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.MnuOpc1:
+                mensaje.setText("Opción 1 Pulsada");
+                return true;
+            case R.id.MnuOpc2:
+                mensaje.setText("Opción 2 Pulsada");
+                return true;
+            case R.id.MnuOpc3:
+                mensaje.setText("Opción 3 Pulsada");
+                return true;
+            case R.id.SubItem1:
+                mensaje.setText("SubItem 1 Pulsada");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
